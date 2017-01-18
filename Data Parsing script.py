c3b72673c0b8a359b251881049af21ee0d713a99
Kanak Singh <kanak.singh@laserfiche.com>
2017-01-18 10:24:35 -0800

    added linear regression datasets, analysis, updated scripts

--- Data Parsing script.py
+++ Data Parsing script.py
@@ -1,85 +1,6 @@
 import csv
 
 '''
-Imports raw data from csv file and returns output in form of list of rows (which are also in list form)
-
-Input:
-filename - a .csv filename in the same directory
-
-Output:
-result - a list of lists, where the inner list elements are the corresponding rows in the csv file
-'''
-def parseCsv(filename):
-	#returns a list of list of strings
-	result = []
-	with open(filename) as csvFile:
-		csvReader = csv.reader(csvFile)
-		for row in csvReader:
-			result += [row]
-	return result
-
-'''
-Builds on top of parseMyCsv by cleaning out the double quotes present inside some string values when data
-is imported from CSV.
-'''
-def cleanParseData(filename):
-	rawData = parseCsv(filename)
-	for i in range(1, len(rawData)):
-		row = rawData[i]
-		for j in range(len(row)):
-			currentItem = row[j].replace('"', '')
-			numericVal = 0
-			if currentItem in general.keys():
-				numericVal = general[currentItem]
-			elif 'job' in rawData[0][j]:
-				numericVal = job[currentItem]
-			elif 'reason' in rawData[0][j]:
-				numericVal = reason[currentItem]
-			elif 'guardian' in rawData[0][j]:
-				numericVal = guardian[currentItem]
-			else:
-				numericVal = float(currentItem)
-			rawData[i][j] = numericVal
-	return rawData
-
-'''
-Prints a list of lists in an orderly fashion.
-It will print each list in the original list in a new line.
-
-Input:
-rowList - a list of lists to be pretty printed
-
-Output:
-<no return value> 
-'''
-def prettyPrint(rowList):
-	for row in rowList:
-		print(row)
-
-def parseTxt(filename):
-	result = []
-	with open(filename) as txtFile:
-		currentRow = []
-		for line in txtFile:
-			itr = 0
-			currentItem = ''
-			while itr < len(line):
-				if line[itr] == ' ' or line[itr] == ',':
-					currentRow += [float(currentItem)]
-					currentItem = ''
-				else:
-					currentItem += line[itr]
-				itr += 1
-			currentRow += [float(currentItem[0:len(currentItem)-1])]
-			result += [currentRow]
-			currentRow = []
-	return result
-
-
-
-parseTxt('linReg_data.txt')
-
-'''
 Following are four dictionaries to map attribute values that are strings into corresponding numbers
 '''
 general = {
@@ -117,3 +38,92 @@ guardian = {
 	'father' : 1,
 	'other' : 2,
 }
+
+'''
+Imports raw data from csv file and returns output in form of list of rows (which are also in list form)
+
+Input:
+filename - a .csv filename in the same directory
+
+Output:
+result - a list of lists, where the inner list elements are the corresponding rows in the csv file
+'''
+def parseCsv(filename):
+	rawData = []
+	with open(filename) as csvFile:
+		csvReader = csv.reader(csvFile)
+		for row in csvReader:
+			rawData += [row]
+	#Perform cleaning and conversion to floats
+	for i in range(1, len(rawData)):
+		row = rawData[i]
+		for j in range(len(row)):
+			currentItem = row[j].replace('"', '')
+			numericVal = 0.0
+			if currentItem in general.keys():
+				numericVal = float(general[currentItem])
+			elif 'job' in rawData[0][j]:
+				numericVal = float(job[currentItem])
+			elif 'reason' in rawData[0][j]:
+				numericVal = float(reason[currentItem])
+			elif 'guardian' in rawData[0][j]:
+				numericVal = float(guardian[currentItem])
+			else:
+				numericVal = float(currentItem)
+			rawData[i][j] = numericVal
+	return rawData
+
+'''
+Prints a list of lists in an orderly fashion.
+It will print each list in the original list in a new line.
+
+Input:
+rowList - a list of lists to be pretty printed
+
+Output:
+<no return value> 
+'''
+def prettyPrint(rowList):
+	for row in rowList:
+		print(row)
+
+def parseTxt(filename):
+	result = []
+	with open(filename) as txtFile:
+		currentRow = []
+		for line in txtFile:
+			itr = 0
+			currentItem = ''
+			currentStringItem = ''
+			stringStarted = False
+			while itr < len(line):
+				if stringStarted: # if you have seen a quotation (") mark already
+					if (line[itr] != '"'):
+						currentItem += line[itr]
+					else:
+						currentRow += [currentItem]
+						currentItem = ''
+				else:	#when you're dealing with floats
+					if (line[itr] == '"'):
+						stringStarted = True
+					else:			
+						if (currentItem != ''):
+							if line[itr] in [' ', ',', '\t']: 
+								currentRow += [float(currentItem)]
+								currentItem = ''
+							else:
+								currentItem += line[itr]
+						else:
+							if not (line[itr] in [' ', '\t', ',']): #sort of extra precaution, to make sure word/item doesn't start with these special chars
+								currentItem += line[itr]
+				itr += 1
+			if len(currentItem) > 1:
+			 	currentRow += [float(currentItem[0:len(currentItem)-1])]
+			result += [currentRow]
+			currentRow = []
+	return result
+
+
+
+#prettyPrint(cleanParseData('CASP.csv'))
+
--- Send Training Data script.py
+++ Send Training Data script.py
@@ -1,5 +1,7 @@
 import json
 import urllib.request, urllib.parse
+import math
+import xlwt
 #import predictionio
 
 dataParsing = __import__("Data Parsing script")
@@ -153,9 +155,9 @@ def testAccuracy(filename):
 	print("hit: " + str(hit))
 	print("miss: " + str(miss))
 
-def sendTrainingData_LinReg(filename, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8FkxRpittj59rLwMKexjplalTEfPrnSn3k"):
-	allData = dataParsing.parseTxt(filename)
-	trainingData = allData[0:int(len(allData)/2)] #no header row
+def sendTrainingData_LinReg(filename, parsingFunction, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8FkxRpittj59rLwMKexjplalTEfPrnSn3k"):
+	allData = parsingFunction(filename)
+	trainingData = allData[1:int(len(allData)/2)] #int(len(allData)/2)
 	currentId = 0
 	for row in trainingData:
 		eId = "u"+ str(currentId)
@@ -172,6 +174,7 @@ def sendTrainingData_LinReg(filename, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8
 				"attr5" : row[6],
 				"attr6" : row[7],
 				"attr7" : row[8],
+				"attr8" : row[9],
 				"plan" : row[0]
 			}
 		}
@@ -180,18 +183,28 @@ def sendTrainingData_LinReg(filename, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8
 		header = {"Content-Type" : "application/json"}
 		req = urllib.request.Request(url, encodedData, header)
 		f = urllib.request.urlopen(req)
-		print(f.read())
 		currentId += 1
+		print(f.read())
 
-def test_LinReg(filename, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8FkxRpittj59rLwMKexjplalTEfPrnSn3k"):
-	allData = dataParsing.parseTxt(filename)
-	trainingData = allData[0:int(len(allData)/2)]
+def test_LinReg(filename, parsingFunction, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8FkxRpittj59rLwMKexjplalTEfPrnSn3k"):
+	allData = parsingFunction(filename)
 	testingData = allData[int(len(allData)/2):len(allData)]
-	for row in trainingData:
-		plan = row[0]
-		expectedOutput = float(plan)
+	len_testingData = len(testingData)
+
+	#testing purposes
+	result = {
+		'error' : 0,
+		'deviationArray' : [],
+		'A_values' : [],
+		'B_values' : [],
+		'expectedValueArray' : [],
+		'actualValueArray' : []
+	}
+	currentRowIdx = 0
+	for row in testingData:
+		expectedValue = float(row[0])
 		data = {
-			"features" : [row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8]]
+			"features" : [row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8], row[9]]
 			}
 		url = 'http://pacora:8000/queries.json'
 		encodedData = json.dumps(data).encode('utf-8')
@@ -200,9 +213,44 @@ def test_LinReg(filename, accessKey="hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8FkxRpittj59r
 		f = urllib.request.urlopen(req)
 		fetchedData = f.read()
 		jsResult = json.loads(fetchedData.decode('utf-8'))
-		actualResult = jsResult['prediction']
-		print("actual result: %f, expected result: %f, abs. difference: %f, perc. difference: %f%%" % (actualResult, expectedOutput, actualResult - expectedOutput, (actualResult - expectedOutput)*100/expectedOutput))
+		actualValue = jsResult['prediction']
+		print("on row %i out of %i. Actual value is %f" % (currentRowIdx, len_testingData, actualValue))
+		#testing
+		result['A_values'] += [row[1]]
+		result['error'] += (actualValue - expectedValue)
+		result['expectedValueArray'] += [expectedValue]
+		result['actualValueArray'] += [actualValue]
+		if (expectedValue == 0):
+			result['deviationArray'] += [math.nan]
+		else:
+			result['deviationArray'] += [(actualValue - expectedValue)*100/expectedValue]
+		currentRowIdx += 1
+	return result
 
-test_LinReg('linReg_data.txt')
+def exporting_LinReg(fileName_input, parsingFunction = dataParsing.parseCsv):
+	result_linReg = test_LinReg(fileName_input, parsingFunction)
+	output_book = xlwt.Workbook()
+	sheet1 = output_book.add_sheet('results')
+	sheet1.write(0,3,"predicted values")
+	sheet1.write(0,2,"A values")
+	for i in range(len(result_linReg['actualValueArray'])):
+		sheet1.write(i+1, 3, result_linReg['actualValueArray'][i])
+		sheet1.write(i+1, 2, result_linReg['A_values'][i])
+	output_book.save('linReg_charts\outputLinReg.xls')
+
+
+
+
+#sendTrainingData_LinReg('auto-mpg.txt', 'hEZk3CwiUZZHBKWm4-gUv8Vv4cerFn8FkxRpittj59rLwMKexjplalTEfPrnSn3k')
+#test_LinReg('auto-mpg.txt')
 #engine_client = predictionio.EngineClient(url="http://pacora:8000")
 #print(engine_client.send_query({"features" :[-1, -2, -1, -3, 0, 0, -1, 0]}))
+
+#sendTrainingData_LinReg('linReg_data.txt', dataParsing.parseTxt)
+#test_LinReg('linReg_data.txt', dataParsing.parseTxt)
+
+#sendTrainingData_LinReg('CASP.csv', dataParsing.parseCsv)
+#test_LinReg('CASP.csv', dataParsing.parseCsv)
+
+exporting_LinReg('CASP.csv')
+
